db9d39a1746bd35b7171acc7639224af975b7043
FraDurazzi <fra.durazzi@gmail.com>
2019-09-04 13:30:02 +0200

    First commit with min_conn_comp

---
+++ min_conn_comp.R
@@ -0,0 +1,22 @@
+#**Based on `igraph`**
+  
+#  This function permits to obtain the minimal subgraph that connects your `sources` nodes. All you have to do is to provide a undirected connected component containing all the sources nodes and the algorithm will detected the shortest path connecting them and collapse these edges into a new subgraph.
+#`igraph` package is required.
+
+min_conn_comp=function(G,sources){
+  D=distances(G,v=sources,to=sources)
+  GD=graph_from_adjacency_matrix(D,mode="undirected",weighted = TRUE)
+  MST=mst(GD)
+  plot.igraph(MST,vertex.label=V(MST)$name,edge.color='black')
+  Gmin=graph.empty(directed=FALSE)
+  for(e in E(MST)){
+    vs=ends(MST,e)
+    print(paste('Computing SP from ',vs[1],' to ',vs[2]))
+    sp=shortest_paths(G,from=vs[1],to=vs[2],output = 'both')
+    newv=sp$vpath
+    print(newv)
+    Gmin=Gmin + vertices(V(G)[newv[[1]]]$name)
+    Gmin=Gmin+path(V(G)[newv[[1]]]$name)
+    Gmin=delete.vertices(simplify(Gmin),degree(Gmin)==0)
+  }
+}
